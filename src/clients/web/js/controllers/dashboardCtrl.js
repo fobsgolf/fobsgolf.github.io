af6fafcd08e29721ea692af986a9d810b02eb1a8
Tuan <nexsovo@gmail.com>
2014-09-05 21:18:48 +1000

    Added menu item to dashboard.

--- src/clients/web/js/controllers/dashboardCtrl.js
+++ src/clients/web/js/controllers/dashboardCtrl.js
@@ -1,4 +1,5 @@
 app.controller("dashboardCtrl", function($scope, $location, graphService) {
     $scope.graphTitle = "Par";
     $scope.series = graphService.getPieStats();
+    $scope.collapsed = true;
 })
